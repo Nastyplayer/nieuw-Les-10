d61d9647872d68154770ca88d0612af9c88b87e1
Nastyplayer <jftalavera@hotmail.com>
2023-02-28 02:35:03 +0100

    bla

---
+++ src/main/java/com/example/easymodel11/exceptions/RecordNotFoundException.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class RecordNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public RecordNotFoundException() {
+        super();
+    }
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+
+
+    public RecordNotFoundException(String no_found, HttpStatus notFound) {
+    }
+}
